0f4b967551f40f8e065cd991b84922091b5ca09d
IT20659608 <akilatharuka555@gmail.com>
2023-04-18 22:58:00 +0530

    Creat Frontend Homepage and Delete Function in BackEnd

--- BackEnd/src/main/java/com/example/demo/controller/MarketPlaceController.java
+++ BackEnd/src/main/java/com/example/demo/controller/MarketPlaceController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,14 +50,14 @@ public class MarketPlaceController {
 		MarketItem marketItem = MarketPlaceRepository.findById(marketItemsID)
 				.orElseThrow(() -> new ResourceNotFoundException("Did not have MarketItemID : " + marketItemsID));
 
-		marketItem.setProductName(marketItem.getProductName());
-		marketItem.setImages(marketItem.getImages());
-		marketItem.setStock(marketItem.getStock());
-		marketItem.setItemCode(marketItem.getItemCode());
-		marketItem.setPrice(marketItem.getPrice());
-		marketItem.setDescription(marketItem.getDescription());
+		marketItem.setProductName(marketItems.getProductName());
+		marketItem.setImages(marketItems.getImages());
+		marketItem.setStock(marketItems.getStock());
+		marketItem.setItemCode(marketItems.getItemCode());
+		marketItem.setPrice(marketItems.getPrice());
+		marketItem.setDescription(marketItems.getDescription());
 
-		MarketItem updatedmarketItem = MarketPlaceRepository.save(marketItem);
+		MarketItem updatedmarketItem = MarketPlaceRepository.save(marketItems);
 		return ResponseEntity.ok(updatedmarketItem);
 	}
 
