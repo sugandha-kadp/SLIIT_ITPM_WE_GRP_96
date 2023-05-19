801774a5cf19d3ce714621b462b62a509ccbbd33
IT20659912 <sanjanapathirane99@gmail.com>
2023-05-19 15:35:26 +0530

    Changed port number in controller

--- BackEnd/src/main/java/com/example/demo/controller/RecipeController.java
+++ BackEnd/src/main/java/com/example/demo/controller/RecipeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,7 @@ import com.example.demo.model.Recipe;
 import com.example.demo.repository.RecipeRepository;
 
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/v1/")
 public class RecipeController {
