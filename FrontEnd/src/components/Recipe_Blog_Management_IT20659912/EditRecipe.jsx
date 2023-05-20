d0b1a600e03263bd60cd4180cc900db8206769c0
IT20659912 <sanjanapathirane99@gmail.com>
2023-05-20 11:43:15 +0530

    modified edit component axios

--- FrontEnd/src/components/Recipe_Blog_Management_IT20659912/EditRecipe.jsx
+++ FrontEnd/src/components/Recipe_Blog_Management_IT20659912/EditRecipe.jsx
@@ -1,58 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './addAdvertisement.css';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useHistory, useParams } from 'react-router-dom';
 
 function RecipeForm() {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [images, setImages] = useState('');
+  const { recipeId } = useParams();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const [recipe, setRecipe] = useState({
+    recipeId: '',
+    title: '',
+    content: '',
+    author: '',
+    images: '',
+  });
 
-    const newItem = {
-      title,
-      content,
-      author,
-      images,
-    };
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/api/v1/Recipe/${recipeId}`)
+      .then((res) => {
+        setRecipe(res.data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }, [recipeId]);
 
-    axios.post('http://localhost:8080/api/v1/Recipe', newItem)
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/Recipes');
+  };
+
+  const handleEdit = () => {
+    axios
+      .put(`http://localhost:8080/api/v1/Recipe/${recipeId}`, recipe)
       .then(() => {
-        alert('Recipe added successfully');
+        alert('Recipe updated successfully');
+        history.push('/Recipes');
       })
       .catch((err) => {
         alert(err);
       });
   };
 
-  const history = useHistory();
-
-    const handleClick = () => {
-      history.push("/Recipes");
-    };
-
   return (
     <div className="advertisement-form-container">
       <h2>Edit Recipe</h2>
-      <form onSubmit={handleSubmit}>
+      <form>
+        <div className="form-group">
+          <label htmlFor="recipeId">Recipe ID:</label>
+          <input
+            type="text"
+            id="recipeId"
+            value={recipe.recipeId}
+            disabled
+          />
+        </div>
         <div className="form-group">
           <label htmlFor="title">Title:</label>
           <input
             type="text"
             id="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            value={recipe.title}
+            onChange={(e) =>
+              setRecipe({ ...recipe, title: e.target.value })
+            }
           />
         </div>
         <div className="form-group">
           <label htmlFor="content">Content:</label>
           <textarea
             id="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            value={recipe.content}
+            onChange={(e) =>
+              setRecipe({ ...recipe, content: e.target.value })
+            }
           ></textarea>
         </div>
         <div className="form-group">
@@ -60,8 +82,10 @@ function RecipeForm() {
           <input
             type="text"
             id="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
+            value={recipe.author}
+            onChange={(e) =>
+              setRecipe({ ...recipe, author: e.target.value })
+            }
           />
         </div>
         <div className="form-group">
@@ -69,16 +93,20 @@ function RecipeForm() {
           <input
             type="text"
             id="image"
-            value={images}
-            onChange={(e) => setImages(e.target.value)}
+            value={recipe.images}
+            onChange={(e) =>
+              setRecipe({ ...recipe, images: e.target.value })
+            }
           />
         </div>
         <button onClick={handleClick}>Return</button>
-        <button type="submit">Submit</button>
-
+        <button type="button" onClick={handleEdit}>
+          Submit
+        </button>
       </form>
     </div>
   );
 }
 
 export default RecipeForm;
+
