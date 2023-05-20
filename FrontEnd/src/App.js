7a9950edebe9966aab09ed54bb44e512c05bf91a
Sugandha-kadp <piyumsugandha@gmail.com>
2023-05-20 12:26:46 +0530

    Add newly created API for Approved Supply requests and Reject Supply requests

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import ListSupplyRequestComponent from './components/Supply_Request_Management_IT20658236/ListSupplyRequestComponent';
-import MarketPlaceComponent from './components/Marketplace_Management_IT20659608/MarketPlaceComponent';
+//import MarketPlaceComponent from './components/Marketplace_Management_IT20659608/MarketPlaceComponent';
 import Home from './components/Common_components/Home';
 import Login from './components/Login/Login';
 import DashBoard from './components/Common_components/DashBoard';
 import AddSupplyRequestFormComponent from './components/Supply_Request_Management_IT20658236/AddSupplyRequestFormComponent';
 import ViewSupplyRequestComponent from './components/Supply_Request_Management_IT20658236/ViewSupplyRequestComponent';
+import SupplyRequestQuotationComponent from './components/Supply_Request_Management_IT20658236/SupplyRequestQuotation';
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
             {/* Supply Request */}
             <Route path="/SupplyRequests" component={ListSupplyRequestComponent}></Route>
             <Route path="/Add-SupplyRequest" component={AddSupplyRequestFormComponent}></Route>
+            <Route path="/SupplyRequest-Quotation/:supplyRequestID" component={SupplyRequestQuotationComponent}></Route>
             <Route path="/view-SupplyRequest/:supplyRequestID" component={ViewSupplyRequestComponent}></Route>
             {/* <Route path="/Update-SupplyRequest" component={ }></Route> */}
             {/* <Route path="/Approved-SupplyRequest" component={ }></Route>
