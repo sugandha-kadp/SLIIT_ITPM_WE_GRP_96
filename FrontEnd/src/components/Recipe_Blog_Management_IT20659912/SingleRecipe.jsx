31ca05a44780a2a869e7b0f189ed6636c4092291
IT20659912 <sanjanapathirane99@gmail.com>
2023-05-20 11:45:45 +0530

    removed unnecessary handle inputs and refactored the code

--- FrontEnd/src/components/Recipe_Blog_Management_IT20659912/SingleRecipe.jsx
+++ FrontEnd/src/components/Recipe_Blog_Management_IT20659912/SingleRecipe.jsx
@@ -1,66 +1,37 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useHistory } from "react-router-dom";
 import './singleview.css'; // Import the CSS file for styling
 
-
-// const SingleRecipe = ({ recipe }) => {
-  
-
 function SingleRecipe(props) {
-  const {id}=useParams();
-    console.log("item id "+id);
+  const { recipeId } = useParams();
+  const [recipe, setRecipe] = useState({});
+  const history = useHistory();
 
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-    const [author, setAuthor] = useState('');
-    const [image, setImages] = useState('');
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/api/v1/Recipe/${recipeId}`)
+      .then((res) => {
+        setRecipe(res.data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }, [recipeId]);
 
-  // const handleTitleChange = (event) => {
-  //   setTitle(event.target.value);
-  // };
-
-  // const handleContentChange = (event) => {
-  //   setContent(event.target.value);
-  // };
-
-  // const handleAuthorChange = (event) => {
-  //   setAuthor(event.target.value);
-  // };
-
-  // const handleImageChange = (event) => {
-  //   setImage(event.target.value);
-  // };
-
-    useEffect(() => {
-        axios.get(`http://localhost:8080/api/v2/Recipe/${id}`).then(res => {
-
-            setTitle(res.data.title);
-            setContent(res.data.content);
-            setAuthor(res.data.author);
-            setImages(res.data.images);
-
-        }).catch((err) => {
-            alert(err.message)
-        })
-    }, [])
-
-    const history = useHistory();
-
-    const handleClick = () => {
-      history.push("/Recipes");
-    };
+  const handleClick = () => {
+    history.push("/Recipes");
+  };
 
   return (
     <div className="advertisement-page">
-      <h2>{title}</h2>
-      <p>{content}</p>
-      <p>Author: {author}</p>
-      <img src={image} alt={title}></img>
+      <h2>{recipe.title}</h2>
+      <p>{recipe.content}</p>
+      <p>Author: {recipe.author}</p>
+      <img src={recipe.images} alt={recipe.title} />
       <button onClick={handleClick}>Back to All Recipes</button>
     </div>
   );
-};
-
+}
 
 export default SingleRecipe;
